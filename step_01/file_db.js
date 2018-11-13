5e6bbe22295cdc0179fdbbe8ccb2425f9d3cf6e2
Student <Student@B15-13.local>
2018-11-13 10:20:22 +0100

    step_01

---
+++ step_01/file_db.js
@@ -0,0 +1,17 @@
+
+let sqlite3 = require('sqlite3').verbose();
+const path = require('path')
+const dbPath = path.resolve(__dirname, 'demodb01')
+let db = new sqlite3.Database(dbPath);
+
+db.serialize(function() {
+    db.run("CREATE TABLE IF NOT EXISTS demo (runtime REAL)");
+
+    db.run("INSERT INTO demo (runtime) VALUES (?)", new Date().getTime());
+
+    db.each("SELECT runtime FROM demo", function(err, row) {
+        console.log("This app was run at " + row.runtime);
+    });
+});
+
+db.close();
